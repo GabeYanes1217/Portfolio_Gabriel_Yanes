c475f94c068ebc96357453b72b7faec2afde8930
GabeYanes1217 <gabriel.yanes1217@outlook.com>
2023-12-04 02:38:52 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+                    }s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
+
+var paragrafo = document.querySelector('h1')
+var cursor = document.querySelector('.cursor')
+var texto = paragrafo.innerHTML
+var index = 0
+
+const escrever = () => {
+  paragrafo.innerHTML = paragrafo.innerHTML.replace('|', '')
+
+  if (texto.length > index) {
+    if (index == 0){
+      paragrafo.innerHTML = texto.charAt(index) 
+    } else {
+      paragrafo.innerHTML += texto.charAt(index) 
+    }
+
+    paragrafo.innerHTML += '|'
+
+    index++
+    setTimeout(escrever, 100)
+
+  } 
+}
+
+escrever()
+
+document.querySelectorAll('li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        var sectionId = this.getAttribute('href');
+        var section = document.querySelector(sectionId);
+        if (section) {
+            var adjustment = section.offsetTop > 0 ? section.offsetTop : 0;
+            setTimeout(function () {
+                window.scrollTo({ top: adjustment, behavior: 'smooth' });
+            }, 100);
+        }
+    });
+});
